8051ab2b337793ecd67dc6d070a998aaf0b0e496
Jonathan Sick <jsick@lsst.org>
2016-05-27 10:55:24 -0700

    Implement JSON pipeline in post-qa command

--- postqa/cli.py
+++ postqa/cli.py
@@ -1,6 +1,43 @@
 """Command line interface / runner."""
 
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+from builtins import *  # NOQA
+from future.standard_library import install_aliases
+install_aliases()  # NOQA
+
+import argparse
+import json
+
+from . import jsonshim
+from . import lsstsw
+
 
 def run_post_qa():
-    """CLI entrypoint for the ``post-qa command."""
-    pass
+    """CLI entrypoint for the ``post-qa`` command."""
+    args = parse_args()
+
+    # Shim validate_drp's JSON to SQuaSH measurements format
+    with open(args.qa_json_path) as f:
+        qa_json = json.load(f, encoding='utf-8')
+    job_json = jsonshim.shim_validate_drp(qa_json)
+
+    # Add 'packages' sub-document
+    lsstsw_install = lsstsw.Lsstsw(args.lsstsw_dirname)
+    job_json.update(lsstsw_install.json)
+    print(json.dumps(job_json, indent=2, sort_keys=True))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--lsstsw',
+        dest='lsstsw_dirname',
+        required=True,
+        help='Path of lsstsw directory')
+    parser.add_argument(
+        '--qa-json',
+        dest='qa_json_path',
+        required=True,
+        help='Filename of QA JSON output file')
+    return parser.parse_args()
