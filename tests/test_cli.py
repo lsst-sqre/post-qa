ab738fdab9df22e13a0b42597ad15fb2ac587f73
Jonathan Sick <jsick@lsst.org>
2016-10-14 16:49:30 -0700

    Add test for upload_json
    This uses the responses package to mock the HTTP post and then check
    that the POST was made.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -7,6 +7,7 @@ from future.standard_library import install_aliases
 install_aliases()  # NOQA
 
 import jsonschema
+import responses
 
 from postqa.schemas import load_squash_job_schema
 import postqa.cli
@@ -14,6 +15,7 @@ import postqa.lsstsw
 
 
 def test_build_job_json(mocker, qa_json_path, lsstsw_dir):
+    # Mock the git repos on the file system
     mocker.patch('postqa.lsstsw.git.Repo')
     postqa.lsstsw.git.Repo.return_value.active_branch.name = 'master'
 
@@ -21,3 +23,26 @@ def test_build_job_json(mocker, qa_json_path, lsstsw_dir):
 
     schema = load_squash_job_schema()
     jsonschema.validate(job_json, schema)
+
+
+@responses.activate
+def test_upload_json(mocker, qa_json_path, lsstsw_dir):
+    api_url = 'https://squash.lsst.codes/api/jobs'
+    api_user = 'user'
+    api_password = 'password'
+
+    # Mock the git repos on the file system
+    mocker.patch('postqa.lsstsw.git.Repo')
+    postqa.lsstsw.git.Repo.return_value.active_branch.name = 'master'
+
+    # mock requests
+    responses.add(responses.POST, api_url,
+                  body='{}', status=201,
+                  content_type='application/json')
+
+    job_json = postqa.cli.build_job_json(qa_json_path, lsstsw_dir)
+
+    postqa.cli.upload_json(job_json, api_url, api_user, api_password)
+
+    assert len(responses.calls) == 1
+    assert responses.calls[0].request.url == api_url
