f6706cc0f5d2a9e3e68b831afe56087f9c46ee1b
Jonathan Sick <jsick@lsst.org>
2016-10-14 15:47:11 -0700

    Refactor test fixtures
    I'm putting common fixtures in tests/conftest.py (a canonical module) so
    that all tests can use them, regardless of the test's module.

---
+++ tests/conftest.py
@@ -0,0 +1,33 @@
+"""Common pytest fixtures."""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+from builtins import *  # NOQA
+from future.standard_library import install_aliases
+install_aliases()  # NOQA
+
+import os
+import json
+import pkg_resources
+import pytest
+
+
+@pytest.fixture
+def lsstsw_dir():
+    """A minimal mock lsstsw/ installation is in /test_data/lsstsw."""
+    return os.path.abspath(
+        os.path.join(os.path.dirname(__file__), '..', 'test_data', 'lsstsw'))
+
+
+@pytest.fixture()
+def vdrp_cfht_output_r(qa_json_path):
+    """test_data/Cfht_output_r.json from validate_drp as a dict."""
+    with open(qa_json_path, encoding='utf-8') as f:
+        json_dict = json.load(f)
+    return json_dict
+
+
+@pytest.fixture()
+def qa_json_path():
+    return pkg_resources.resource_filename(
+        __name__, '../test_data/Cfht_output_r.json')
--- tests/test_jsonshim.py
+++ tests/test_jsonshim.py
@@ -6,29 +6,10 @@ from builtins import *  # NOQA
 from future.standard_library import install_aliases
 install_aliases()  # NOQA
 
-import os
-import json
-import pkg_resources
-import pytest
 import jsonschema
 
 from postqa.jsonshim import shim_validate_drp
 from postqa.schemas import load_squash_measurements_schema
-
-
-def load_test_data(filename):
-    resource_args = (__name__, os.path.join('../test_data/', filename))
-    assert pkg_resources.resource_exists(*resource_args)
-    data_str = pkg_resources.resource_string(*resource_args)
-    return data_str
-
-
-@pytest.fixture()
-def vdrp_cfht_output_r():
-    """test_data/Cfht_output_r.json from validate_drp as a dict."""
-    json_str = load_test_data('Cfht_output_r.json').decode('utf-8')
-    json_dict = json.loads(json_str)
-    return json_dict
 
 
 def test_measurements_schema(vdrp_cfht_output_r):
