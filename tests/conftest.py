269558b04cab72c347b8bf6b7863602d08f41559
Jonathan Sick <jsick@lsst.org>
2016-10-15 01:22:09 -0700

    Move test data to tests/data
    Also change access patterns in test code:
    
    - Move manifest fixture to conftest.py
    - Don't use pkg_resources anymore, not building paths from __file__
      works just fine.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,21 +7,30 @@ from future.standard_library import install_aliases
 install_aliases()  # NOQA
 
 import os
+import io
 import json
-import pkg_resources
 import pytest
 
 
 @pytest.fixture
 def lsstsw_dir():
     """A minimal mock lsstsw/ installation is in /test_data/lsstsw."""
-    return os.path.abspath(
-        os.path.join(os.path.dirname(__file__), '..', 'test_data', 'lsstsw'))
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(test_dir, 'data', 'lsstsw')
+
+
+@pytest.fixture()
+def manifest(lsstsw_dir):
+    """Sample manifest file object.."""
+    manifest_path = os.path.join(lsstsw_dir, 'build', 'manifest.txt')
+    with open(manifest_path, encoding='utf-8') as f:
+        manifest_data = f.read()
+    return io.StringIO(manifest_data)
 
 
 @pytest.fixture()
 def vdrp_cfht_output_r(qa_json_path):
-    """test_data/Cfht_output_r.json from validate_drp as a dict."""
+    """data/Cfht_output_r.json from validate_drp as a dict."""
     with open(qa_json_path, encoding='utf-8') as f:
         json_dict = json.load(f)
     return json_dict
@@ -29,5 +38,6 @@ def vdrp_cfht_output_r(qa_json_path):
 
 @pytest.fixture()
 def qa_json_path():
-    return pkg_resources.resource_filename(
-        __name__, '../test_data/Cfht_output_r.json')
+    """Path to the sample validate_drp dataset."""
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(test_dir, 'data', 'Cfht_output_r.json')
--- tests/test_pkgdata.py
+++ tests/test_pkgdata.py
@@ -6,27 +6,7 @@ from builtins import *  # NOQA
 from future.standard_library import install_aliases
 install_aliases()  # NOQA
 
-import os
-import io
-import pkg_resources
-import pytest
-
 from postqa.pkgdata import Manifest
-
-
-def load_test_data(filename):
-    resource_args = (__name__,
-                     os.path.join('../test_data/lsstsw/build', filename))
-    assert pkg_resources.resource_exists(*resource_args)
-    data_str = pkg_resources.resource_string(*resource_args).decode('utf-8')
-    return data_str
-
-
-@pytest.fixture()
-def manifest():
-    """test_data/manifest.txt"""
-    manifest_data = load_test_data('manifest.txt')
-    return io.StringIO(manifest_data)
 
 
 def test_manifest(manifest):
